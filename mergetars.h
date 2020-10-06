cf6306afe3e37cfde790ae70b7eee2464cf9b048
Haxinator <joshuarachpaul@hotmail.com>
2020-10-06 15:18:19 +0800

    Added header file

---
+++ mergetars.h
@@ -0,0 +1,15 @@
+
+//welcome to the main headerfile
+
+//include these bad boys in our files
+#include <stdio.h>
+#include <stdlib.h>
+
+//add any #define//
+
+
+//add function declartions//
+
+
+//add any global variables//
+
