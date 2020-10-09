7602d17b199ed39ee23cba2b7098480c7a8bd5df
Haxinator <joshuarachpaul@hotmail.com>
2020-10-09 14:26:37 +0800

    Updated

--- mergetars.c
+++ mergetars.c
@@ -1,9 +1,17 @@
-
+#include "mergetars.h"
 
 //look at my comment very nice//
 
 int main(int argsc, char *argsv[])
 {
+    //FAILURE CONDITION//
+    if(argsc<3)
+    {
+        fprintf(stderr, "Program expects 3 arguments recieved %i", argsc);
+        exit(EXIT_FAILURE);
+    }
 
-    return 0;
+    //TODO ADD FUNCTIONS//
+
+    exit(EXIT_SUCCESS);
 }
