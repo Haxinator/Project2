0b4b51052b1dd125b1128584a84b3c2ffcf3bd6c
Haxinator <joshuarachpaul@hotmail.com>
2020-10-09 14:33:22 +0800

    Added createdir.c

---
+++ createdir.c
@@ -0,0 +1,10 @@
+#include "mergetars.h"
+
+//Creates the temporary directories//
+//Temproary directories are used to store the extracted contents of tar file//
+
+void createdir(int n,char array*)
+{
+    
+}
+
